a9e414220b9c5ac01e41806e839d0db6cf098034
Brendan Murphy <bjohnmurphy1984@gmail.com>
2016-05-08 10:57:24 -0400

    Various updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
-  console.log("Ready!");
   var mybodyid = $('.this_should_be_the_body').attr('id');
   var mynavid = '#nav' + mybodyid;
-  console.log("mybodyid is " + mybodyid);
-  console.log("mynavid is " + mynavid);
   $(mynavid).attr('id', 'iamhere');
 });
