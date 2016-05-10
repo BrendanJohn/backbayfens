eaea34a032f2ba8f690fdd766c2003dd7d8c3910
Brendan Murphy <bjohnmurphy1984@gmail.com>
2016-05-09 20:40:03 -0400

    Added lots and lots of stuff, images, css etx

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
+"use strict"; 
+
 $(document).ready(function() {
-  var mybodyid = $('.this_should_be_the_body').attr('id');
+  var mybodyid = $('.body').attr('id');
   var mynavid = '#nav' + mybodyid;
   $(mynavid).attr('id', 'iamhere');
 });
